5b8b978d97ca993c4b5fb7f4b7dc58d32f15d871
evellyn pereira lacerda matos <evellynl464@gmail.com>
2022-12-12 00:16:03 -0300

    projeto final do primeiro módulo

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Receba o valor de troco da sua compra");
+
+Console.WriteLine("Digite o valor total da compra:");
+string valortotal = Console.ReadLine()!;
+decimal um = Convert.ToDecimal(valortotal);
+
+Console.WriteLine("Digite o valor pago:");
+string valordpago = Console.ReadLine()!;
+decimal dois = Convert.ToDecimal(valordpago);
+
+decimal troco = um - dois;
+
+if (dois >= um)
+{
+
+Console.WriteLine("Troco:");
+
+}
+else if ( dois < um)
+{
+
+Console.WriteLine("Deve pagar:");
+
+}
+
+Console.WriteLine($"{troco}");
